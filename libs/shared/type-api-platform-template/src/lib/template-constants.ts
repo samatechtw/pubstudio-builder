87be1f578ca34bde38f100b2ce53adb8f0166e9b
Sam Pullman <sampullman@gmail.com>
2023-11-05 14:31:54 +0800

    refactor: adjustments for frontend template work

---
+++ libs/shared/type-api-platform-template/src/lib/template-constants.ts
@@ -0,0 +1,15 @@
+export const GLOBAL_TEMPLATE_COLLECTION_ID = '1149dbf6-7773-4d0c-803b-971bda422d16'
+
+export const ALLOWED_CATEGORIES = [
+  'Personal',
+  'Portfolio',
+  'Blog',
+  'Business',
+  'News',
+  'Entertainment',
+  'Educational',
+  'Aggregator',
+  'Community',
+  'Wiki',
+  'Knowledge Base',
+]
