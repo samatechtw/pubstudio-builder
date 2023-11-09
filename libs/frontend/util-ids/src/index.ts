165784296052e50c1d6867852fa4475af8225d3b
Sam Pullman <sampullman@gmail.com>
2023-11-10 02:21:14 +0800

    fix: various ui bugs

--- libs/frontend/util-ids/src/index.ts
+++ libs/frontend/util-ids/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/builtin-ids'
 export * from './lib/generate-ids'
 export * from './lib/builtin-theme-variables'
-export * from './lib/builtin-fonts'
 export * from './lib/global-context'
 export * from './lib/default-breakpoint-id'
--- libs/frontend/util-ids/src/lib/builtin-fonts.ts
+++
@@ -1,4 +0,0 @@
-export const builtinFonts = {
-  'font-title': 'Helvetica, sans-serif',
-  'font-text': 'Georgia, sans-serif',
-}
