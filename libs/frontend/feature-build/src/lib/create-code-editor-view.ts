57151a41822d49102ae7de83ad6b3e66a070574f
Steven Chiu <steven@samatech.tw>
2023-12-09 10:22:01 +0800

    fix: behavior modal javascript highlighting bug

--- libs/frontend/feature-build/src/lib/create-code-editor-view.ts
+++ libs/frontend/feature-build/src/lib/create-code-editor-view.ts
@@ -3,22 +3,10 @@ import {
   prosemirrorSetup,
   schemaCode,
 } from '@pubstudio/frontend/util-edit-text'
-import hljs from 'highlight.js/lib/core'
-import { keymap } from 'prosemirror-keymap'
-import { Plugin, Selection } from 'prosemirror-state'
+import { Selection } from 'prosemirror-state'
 import { EditorView } from 'prosemirror-view'
-import { highlightPlugin } from './prosemirror-highlighthjs-plugin'
-
-const codeEditorPlugins: Plugin[] = [
-  keymap({
-    // Insert 2 spaces in the selected position when tab is pressed.
-    Tab: (state, dispatch) => {
-      dispatch?.(state.tr.insertText('  ').scrollIntoView())
-      return true
-    },
-  }),
-  highlightPlugin(hljs),
-]
+import { Slice, Fragment } from 'prosemirror-model'
+import { codeEditorPlugins } from './code-editor-plugins'
 
 export function createCodeEditorView(
   code: string,
@@ -42,21 +30,20 @@ export function createCodeEditorView(
       const { textContent } = newState.doc
       if (newState.doc.content.firstChild?.type.name !== 'code_block') {
         // When using hljs with ProseMirror editor, the code must be wrapped inside
-        // `<pre><code>...</code></pre>` for highlighting&styling feature to work. If a user
-        // removes all the text from the editor (i.e. select all text then press backspace),
+        // `<pre><code>...</code></pre>`, which corresponds to the `code_block` node
+        // in the schema for highlighting&styling feature to work.
+        // If a user removes all the text from the editor (i.e. select all text then press backspace),
         // the leading&ending <pre><code> well also be removed, causing the highlighting & styling
-        // feature to stop working. We need to manually wrap the content inside <pre><code> in such cases.
-
-        // FIXME
-        // 1. The current solution will clear the history because it creates a brand-new editor state.
-        //    Find a way to replace the content in the editor state while keeping everything.
-        // 2. New line characters are ignored if the text is copied from a source other than ProseMirror
-        //    editor. For example, VSCode.
-        newState = prosemirrorSetup({
-          schema: schemaCode,
-          content: `<pre><code>${textContent}</code></pre>`,
-          plugins: [...codeEditorPlugins],
-        })
+        // feature to stop working. So we need to manually add the `code_block` node back in such cases.
+        const codeBlockNode = schemaCode.node(
+          'code_block',
+          undefined,
+          schemaCode.text(textContent),
+        )
+        const slice = new Slice(Fragment.from(codeBlockNode), 0, 0)
+        newState = newState.apply(
+          newState.tr.replaceRange(0, newState.doc.content.size, slice),
+        )
         newState.selection = Selection.atEnd(newState.doc)
       }
       view.updateState(newState)
