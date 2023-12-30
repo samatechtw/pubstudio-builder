526fcd06363ce9dbea378760b4930a897d3710ea
Sam Pullman <sampullman@gmail.com>
2023-12-30 15:38:35 +0800

    fix: live mode mailto and other link bugs #184

--- libs/frontend/util-router/src/lib/href-to-url.spec.ts
+++ libs/frontend/util-router/src/lib/href-to-url.spec.ts
@@ -4,7 +4,9 @@ describe('instantiates an URL instance from the given href', () => {
   it('should work when href starts with http', () => {
     const href =
       'http://tmp.com/abc?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C#test-c-1'
-    const { pathname, search, hash } = hrefToUrl(href)
+    const { isExternal, url } = hrefToUrl(href)
+    const { pathname, search, hash } = new URL(url)
+    expect(isExternal).toBe(true)
     expect(pathname).toEqual('/abc')
     expect(search).toEqual('?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C')
     expect(hash).toEqual('#test-c-1')
@@ -13,7 +15,9 @@ describe('instantiates an URL instance from the given href', () => {
   it('should work when href starts with https', () => {
     const href =
       'https://tmp.com/abc?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C#test-c-1'
-    const { pathname, search, hash } = hrefToUrl(href)
+    const { isExternal, url } = hrefToUrl(href)
+    const { pathname, search, hash } = new URL(url)
+    expect(isExternal).toBe(true)
     expect(pathname).toEqual('/abc')
     expect(search).toEqual('?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C')
     expect(hash).toEqual('#test-c-1')
@@ -22,7 +26,9 @@ describe('instantiates an URL instance from the given href', () => {
   it('should work when href starts with www.', () => {
     const href =
       'www.tmp.com/abc?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C#test-c-1'
-    const { pathname, search, hash } = hrefToUrl(href)
+    const { isExternal, url } = hrefToUrl(href)
+    const { pathname, search, hash } = new URL(url)
+    expect(isExternal).toBe(true)
     expect(pathname).toEqual('/abc')
     expect(search).toEqual('?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C')
     expect(hash).toEqual('#test-c-1')
@@ -30,7 +36,9 @@ describe('instantiates an URL instance from the given href', () => {
 
   it('should work when href is a relative path with leading slash', () => {
     const href = '/abc?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C#test-c-1'
-    const { pathname, search, hash } = hrefToUrl(href)
+    const { isExternal, url } = hrefToUrl(href)
+    const { pathname, search, hash } = new URL(url)
+    expect(isExternal).toBe(false)
     expect(pathname).toEqual('/abc')
     expect(search).toEqual('?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C')
     expect(hash).toEqual('#test-c-1')
@@ -38,7 +46,9 @@ describe('instantiates an URL instance from the given href', () => {
 
   it('should work when href is a relative path without leading slash', () => {
     const href = 'abc?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C#test-c-1'
-    const { pathname, search, hash } = hrefToUrl(href)
+    const { isExternal, url } = hrefToUrl(href)
+    const { pathname, search, hash } = new URL(url)
+    expect(isExternal).toBe(false)
     expect(pathname).toEqual('/abc')
     expect(search).toEqual('?a=b&c=d&keyword=%E5%93%88%E5%9B%89%E4%B8%96%E7%95%8C')
     expect(hash).toEqual('#test-c-1')
