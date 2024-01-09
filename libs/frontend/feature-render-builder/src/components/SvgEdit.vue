2fce2c39ac34c7d4351f966dd1c97e7cda44625e
Steven Chiu <steven@samatech.tw>
2024-01-09 15:30:16 +0800

    fix: svg absolute position bug #207

---
+++ libs/frontend/feature-render-builder/src/components/SvgEdit.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="svg-edit">
+    <Edit color="white" class="edit-icon" @click="editSvg" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Edit } from '@pubstudio/frontend/ui-widgets'
+import { setEditSvg } from '@pubstudio/frontend/util-command'
+import { useBuild } from '@pubstudio/frontend/feature-build'
+
+const { editor } = useBuild()
+
+const editSvg = () => {
+  if (editor.value && editor.value.selectedComponent) {
+    setEditSvg(editor.value, { content: editor.value.selectedComponent.content ?? '' })
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+@import '@theme/css/mixins.postcss';
+
+.svg-edit {
+  @mixin flex-center;
+  position: relative;
+  margin-top: -100%;
+  .edit-icon {
+    position: absolute;
+    top: -32px;
+    right: -24px;
+    width: 26px;
+    height: 26px;
+  }
+}
+</style>
