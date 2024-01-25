abd288bf1927f72f2069c4b9b4b98750b630e4a4
Sam Pullman <sampullman@gmail.com>
2024-01-26 03:08:15 +0800

    fix: move link tooltip down if above editor

--- libs/frontend/feature-render-builder/src/components/SvgEdit.vue
+++ libs/frontend/feature-render-builder/src/components/SvgEdit.vue
@@ -1,21 +1,47 @@
 <template>
-  <div class="svg-edit">
+  <div class="svg-edit" :style="{ top: `${offsetTop}px` }">
     <Edit color="black" class="edit-icon" @click="editSvg" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref, toRefs, watch } from 'vue'
 import { Edit } from '@pubstudio/frontend/ui-widgets'
 import { setEditSvg } from '@pubstudio/frontend/util-command'
 import { useBuild } from '@pubstudio/frontend/feature-build'
 
-const { editor } = useBuild()
+const { editor, site } = useBuild()
+const offsetTop = ref(-28)
+
+const props = defineProps<{
+  componentId: string
+}>()
+const { componentId } = toRefs(props)
 
 const editSvg = () => {
   if (editor.value && editor.value.selectedComponent) {
     setEditSvg(editor.value, { content: editor.value.selectedComponent.content ?? '' })
   }
 }
+
+const computeOffset = () => {
+  const editorWindow = document
+    .getElementById('build-content-window')
+    ?.getBoundingClientRect()
+  const component = document.getElementById(componentId.value)?.getBoundingClientRect()
+  if (editorWindow && component) {
+    const pushDown = component.top - editorWindow.top < 30
+    offsetTop.value = pushDown ? component.height - 20 : -28
+  }
+}
+
+watch(() => site.value.context.components?.[componentId.value], computeOffset, {
+  deep: true,
+})
+
+onMounted(() => {
+  computeOffset()
+})
 </script>
 
 <style lang="postcss" scoped>
@@ -24,7 +50,6 @@ const editSvg = () => {
 .svg-edit {
   @mixin flex-center;
   position: absolute;
-  top: -28px;
   right: -28px;
   .edit-icon {
     width: 26px;
