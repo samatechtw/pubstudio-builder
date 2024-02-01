977a92ac70f0b48cfcedad34484fe6153ea5554b
Claire <claire.mao@samatech.tw>
2024-02-01 15:55:53 +0800

    fix: list-site-servers current_load incorrect #1000

---
+++ libs/shared/util-test/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'shared-util-test',
+  preset: '../../../jest.preset.ts',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/shared/util-test',
+}
---
+++ libs/shared/util-test/src/lib/get-site-capacity.ts
@@ -0,0 +1,16 @@
+import { SiteType } from '@pubstudio/shared/type-api-platform-site'
+
+export function getCapacity(siteType: SiteType): number {
+  switch (siteType) {
+    case SiteType.Free:
+      return 1
+    case SiteType.Paid1:
+      return 5
+    case SiteType.Paid2:
+      return 20
+    case SiteType.Paid3:
+      return 50
+    default:
+      throw new Error('Invalid SiteType')
+  }
+}
