5fc3c65dc5663c4e318b962bd3bba65b938dd5b0
Sam Pullman <sampullman@gmail.com>
2024-04-08 13:34:04 +0800

    feat: custom table remove column

--- apps/api-site-test/test/mocks/mock-add-column-payload.ts
+++ apps/api-site-test/test/mocks/mock-add-column-payload.ts
@@ -1,18 +1,16 @@
 export const mockAddColumnPayload1 = () => {
-  return JSON.parse(`
-  {
-    "table_name": "contact_form",
-    "column": {
-        "phone": {
-            "data_type": "TEXT",
-            "validation_rules": [
-                {
-                    "rule_type": "MaxLength",
-                    "parameter": 10
-                }
-            ]
-        }
-    }
+  return {
+    table_name: 'contact_form',
+    column: {
+      phone: {
+        data_type: 'TEXT',
+        validation_rules: [
+          {
+            rule_type: 'MaxLength',
+            parameter: 10,
+          },
+        ],
+      },
+    },
   }
-  `)
 }
--- backend/site-api/src/app/custom/remove_column.rs
+++ backend/site-api/src/app/custom/remove_column.rs
@@ -1,11 +1,17 @@
 use lib_shared_site_api::error::{api_error::ApiError, helpers::check_bad_form};
-use lib_shared_types::dto::custom_data::remove_column_dto::RemoveColumn;
+use lib_shared_types::dto::custom_data::{
+    custom_data_info_viewmodel::{to_api_response, CustomDataInfoViewModel},
+    remove_column_dto::RemoveColumn,
+};
 use serde_json::Value;
 use validator::Validate;
 
 use crate::api_context::ApiContext;
 
-use super::custom_data::parse_request_data;
+use super::{
+    custom_data::parse_request_data,
+    helpers::{get_column_info, save_column_info},
+};
 
 /*
 {
@@ -20,9 +26,24 @@ pub async fn remove_column(
     context: &ApiContext,
     site_id: &String,
     data: Value,
-) -> Result<(), ApiError> {
+) -> Result<CustomDataInfoViewModel, ApiError> {
     let dto: RemoveColumn = parse_request_data(data)?;
     check_bad_form(dto.validate())?;
 
-    Ok(())
+    let table = &dto.table_name.clone();
+    let column_to_remove = dto.column_name.clone();
+
+    context
+        .custom_data_repo
+        .remove_column(site_id, &dto)
+        .await
+        .map_err(|e| ApiError::internal_error().message(e))?;
+
+    // Update columns in custom_data_info table
+    let mut original_columns = get_column_info(context, site_id, table).await?;
+    original_columns.remove(&column_to_remove);
+
+    let result = save_column_info(context, site_id, table, original_columns).await?;
+
+    Ok(to_api_response(result))
 }
