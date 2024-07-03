61d71a91d7e7bcb64643f4a0968dba4a8226cdff
Sam Pullman <sampullman@gmail.com>
2024-07-03 14:18:11 +0800

    fix: site settings ui bugs #516

--- libs/frontend/ui-widgets/src/lib/UsageProgress.vue
+++ libs/frontend/ui-widgets/src/lib/UsageProgress.vue
@@ -2,9 +2,10 @@
   <div class="usage-wrap">
     <div class="usage">
       <div class="bar-bg"></div>
-      <div class="bar" :style="{ width: `${100 * (current / max)}%` }"></div>
+      <div class="bar" :style="{ width: progress }"></div>
     </div>
-    <div class="usage-text">
+    <Spinner v-if="loading" :size="9" color="#3a86ff" class="usage-loading" />
+    <div v-else class="usage-text">
       {{ progressText }}
     </div>
   </div>
@@ -13,19 +14,34 @@
 <script lang="ts" setup>
 import { computed, toRefs } from 'vue'
 import { formatBytes } from '@pubstudio/shared/util-format'
+import { Spinner } from '@pubstudio/frontend/ui-widgets'
 
-const props = defineProps<{
-  // Current usage in bytes
-  current: number
-  // Max allowed usage
-  max: number
-}>()
-const { current, max } = toRefs(props)
+const props = withDefaults(
+  defineProps<{
+    loading: boolean
+    // Current usage in bytes
+    current: number | undefined
+    // Max allowed usage
+    max: number | undefined
+  }>(),
+  {
+    current: 0,
+    max: 100,
+  },
+)
+const { current, max, loading } = toRefs(props)
 
 const progressText = computed(() => {
   const curStr = formatBytes(current.value)
   const maxStr = formatBytes(max.value)
   return `${curStr} / ${maxStr}`
+})
+
+const progress = computed(() => {
+  if (loading.value) {
+    return '0%'
+  }
+  return `${100 * (current.value / max.value)}%`
 })
 </script>
 
@@ -53,5 +69,9 @@ const progressText = computed(() => {
 .usage-text {
   @mixin text 15px;
   margin-top: 8px;
+}
+.usage-loading {
+  height: 20px;
+  margin-top: 7px;
 }
 </style>
