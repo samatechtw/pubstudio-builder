f01e471eda39ba5f4aa072f434f0d03b31419f88
Sam Pullman <sampullman@gmail.com>
2024-10-02 07:41:12 +0800

    refactor: disable some broken eslint rules

--- libs/frontend/feature-render-builder/.eslintrc.cjs
+++ libs/frontend/feature-render-builder/.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['../../../tools/web.eslintrc.cjs'],
   rules: {
+    '@typescript-eslint/no-duplicate-enum-values': 'off',
     'vue/multi-word-component-names': [
       'error',
       {
