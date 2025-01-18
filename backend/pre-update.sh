20da787e13661a71f12c12b68c8aba2874e22531
Sam Pullman <sampullman@gmail.com>
2025-01-19 00:43:25 +0800

    feat: improve site-api pre-update script #536

--- backend/pre-update.sh
+++ backend/pre-update.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 echo "Running pre-update script"
-if curl http://localhost:3100/api/actions/persist-usage; then
-    sleep 5
+URL="http://localhost:3100/api/actions/persist-usage"
+response=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+
+if [ $? -ne 0 ]; then
+    echo "Error: Unable to reach $URL"
+    exit 1
+fi
+
+if [ "$response" -eq 200 ]; then
     echo "persist-usage succeeded"
+    sleep 1
 else
-    echo "persist-usage failed, continuing..."
-    sleep 5
+    echo "persist-usage failed, skipping update"
+    sleep 1
     exit 1
 fi
 echo "Pre update completed"
