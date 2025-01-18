7875e76ecb2e19db12d5131278129ed728cd18b9
Sam Pullman <sampullman@gmail.com>
2025-01-19 00:02:32 +0800

    refactor: docker pre-update error exit #536

--- backend/pre-update.sh
+++ backend/pre-update.sh
@@ -2,9 +2,12 @@
 
 echo "Running pre-update script"
 if curl http://localhost:3100/api/actions/persist-usage; then
+    sleep 5
     echo "persist-usage succeeded"
 else
     echo "persist-usage failed, continuing..."
+    sleep 5
+    exit 1
 fi
 echo "Pre update completed"
 exit 0
